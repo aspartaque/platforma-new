6d3cd9542dc5e9d5562125a34836a00642389ee7
aspartaue <spartakvasilev1999@gmail.com>
2021-12-15 01:09:57 +0900

    finished settings

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,28 @@
-function defaultTask(cb) {
-  // place code for your default task here
-  cb();
+const { src, dest, watch } = require('gulp')
+const livereload = require('gulp-livereload')
+const scss = require('gulp-sass')(require('sass'));
+const express = require('express')
+const app = express()
+
+function compileStyles () {
+    return src('./src/style.scss')
+        .pipe(scss())
+        .pipe(dest('./public/dist/styles/'))
+        .pipe(livereload())
 }
 
-exports.default = defaultTask
+function startServer () {
+    app.use(express.static('public'))
+
+    app.listen(3000, function () {
+        console.log('server is running')
+    })
+}
+
+exports.reload = function () {
+    livereload.listen()
+
+    watch('./src/*.scss', compileStyles)
+
+    startServer()
+}
