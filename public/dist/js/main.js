c0a8227c87dfba7d1abac245ff802f528b8bf34a
spxrtxk <spartakvasilev1999@gmail.com>
2021-12-23 13:56:06 +0900

    gossector

--- public/dist/js/main.js
+++ public/dist/js/main.js
@@ -16,3 +16,49 @@ $(function () {
   });
 });
 
+/*nums*/
+
+var stop = false;
+
+function numStart(self, endNumber) {
+  $(self).animate({numberValue: endNumber}, {
+    duration: 1000,
+    easing: "linear",
+    step: function(val) {
+      $(self).html(Math.ceil(val));     
+    }
+  });
+}
+
+function scrollStart() {
+  var height = $(window).scrollTop();
+  if(height >= 6600){
+    $(".js-num").each(function(item) {
+      var i = parseFloat($(this).text());
+      numStart(this, i);
+    });
+    $(".js-num").addClass('opacity');
+    stop = true;
+  }
+}
+$(window).scroll(function() {
+  if (stop == false) {
+    scrollStart();
+  }
+});
+
+
+$(window).on('scroll', function() {
+  // console.log( $(this).scrollTop() );
+});
+
+
+// var hash = window.location.hash;
+// var href = $(".l-categories__buttons-link-href").attr('href');
+// console.log(href);
+// console.log(hash);
+
+// if (hash == href) {
+//   $(".l-categories__buttons-link").addClass('active');
+// }
+
