17298d3c0d3ba78f4fa3cd0b74afc9b9988dbede
spxrtxk <spartakvasilev1999@gmail.com>
2022-02-01 15:41:12 +0900

    new design

--- public/dist/js/main.js
+++ public/dist/js/main.js
@@ -32,28 +32,32 @@ $(window).scroll(function() {
 });
 
 
-$(window).on('scroll', function() {
-  // console.log( $(this).scrollTop() );
-});
+// $(window).on('scroll', function() {
+//   if ($(this).scrollTop() >= 600) {
+//     $('.header').addClass('fixed');
+//   } else {
+//     $('.header').removeClass('fixed');
+//   }
+//   // console.log( $(this).scrollTop() );
+// });
 
 
-/*header fixed*/
-var h = $(window).scrollTop();
-$(window).scroll(function() {
-  if (h >= 600) {
-    $(".header").addClass('sticky-header');
-  };
-});
-
-
-// var hash = window.location.hash;
-// var href = $(".l-categories__buttons-link-href").attr('href');
-// console.log(href);
-// console.log(hash);
-
-// if (hash == href) {
-//   $(".l-categories__buttons-link").addClass('active');
+// const scrollToHash = urlHash => {
+//   const headerOffset = 90 // полная высота header'a
+//   const contentAnchors = document.querySelectorAll("#item")
+//   const elementToScroll = Array.from(contentAnchors).filter(item => 
+//       item.getAttribute("href") === urlHash)
+//   const elementPosition = elementToScroll[0].offsetTop
+//   window.scrollTo({ top: elementPosition - headerOffset })
 // }
+
+// (() => {
+//   const url = new URL(window.location)
+//   const urlHash = url.hash
+//   if (urlHash) {
+//     scrollToHash(urlHash.substr(1)) // убираем символ #
+//   }
+// })()
 
 /*slider*/
 var swiper = new Swiper(".mySwiper-vertical", {
@@ -87,3 +91,66 @@ var swiper = new Swiper(".mySwiper", {
     prevEl: ".swiper-button-prev",
   },
 });
+
+/* HOVERS */
+
+/* Store the element in el */
+document.querySelectorAll('#item').forEach(el => {
+
+
+/* Get the height and width of the element */
+const height = el.clientHeight
+const width = el.clientWidth
+
+/*
+  * Add a listener for mousemove event
+  * Which will trigger function 'handleMove'
+  * On mousemove
+  */
+el.addEventListener('mousemove', handleMove)
+
+/* Define function a */
+function handleMove(e) {
+  /*
+    * Get position of mouse cursor
+    * With respect to the element
+    * On mouseover
+    */
+  /* Store the x position */
+  const xVal = e.layerX
+  /* Store the y position */
+  const yVal = e.layerY
+  
+  /*
+    * Calculate rotation valuee along the Y-axis
+    * Here the multiplier 20 is to
+    * Control the rotation
+    * You can change the value and see the results
+    */
+  const yRotation = 8 * ((xVal - width / 2) / width)
+  
+  /* Calculate the rotation along the X-axis */
+  const xRotation = -8 * ((yVal - height / 2) / height)
+  
+  /* Generate string for CSS transform property */
+  const string = 'perspective(500px) scale(1) rotateX(' + xRotation + 'deg) rotateY(' + yRotation + 'deg)'
+  
+  /* Apply the calculated transformation */
+  el.style.transform = string
+}
+
+/* Add listener for mouseout event, remove the rotation */
+el.addEventListener('mouseout', function() {
+  el.style.transform = 'perspective(500px) scale(1) rotateX(0) rotateY(0)'
+})
+
+/* Add listener for mousedown event, to simulate click */
+el.addEventListener('mousedown', function() {
+  el.style.transform = 'perspective(500px) scale(1) rotateX(0) rotateY(0)'
+})
+
+/* Add listener for mouseup, simulate release of mouse click */
+el.addEventListener('mouseup', function() {
+  el.style.transform = 'perspective(500px) scale(1) rotateX(0) rotateY(0)'
+})
+})
